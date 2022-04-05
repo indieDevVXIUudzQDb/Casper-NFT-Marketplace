1dc18388e2de87cd22075c9d6758c15abbb82a51
indieDevVXIUudzQDb <tokyoIndieDev@outlook.com>
2022-04-05 18:02:45 +0900

    restore nft contract boilerplate

--- contracts/cep-market/contract/src/event.rs
+++ contracts/cep-market/contract/src/event.rs
@@ -7,5 +7,22 @@ pub enum MarketEvent {
     Mint {
         recipient: Key,
         token_ids: Vec<TokenId>,
-    }
+    },
+    Burn {
+        owner: Key,
+        token_ids: Vec<TokenId>,
+    },
+    Approve {
+        owner: Key,
+        spender: Key,
+        token_ids: Vec<TokenId>,
+    },
+    Transfer {
+        sender: Key,
+        recipient: Key,
+        token_ids: Vec<TokenId>,
+    },
+    MetadataUpdate {
+        token_id: TokenId,
+    },
 }
