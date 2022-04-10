22344e9b91cf638cd797f2c2a5d1ede0d3be943e
indieDevVXIUudzQDb <tokyoIndieDev@outlook.com>
2022-04-10 09:14:39 +0900

    renaming

--- contracts/cep-market/tests/src/market_tests.rs
+++ contracts/cep-market/tests/src/market_tests.rs
@@ -39,47 +39,47 @@ fn get_nft_contract_hash() -> ContractHash {
 fn deploy(meta: Meta) -> (TestEnv, MarketContractInstance, AccountHash) {
     let env = TestEnv::new();
     let market_owner = env.next_user();
-    let item = MarketContractInstance::new(&env, NAME, market_owner, NAME, SYMBOL, meta);
-    (env, item, market_owner)
+    let market_contract = MarketContractInstance::new(&env, NAME, market_owner, NAME, SYMBOL, meta);
+    (env, market_contract, market_owner)
 }
 
 
 #[test]
 fn test_deploy() {
-    let (_, item, _) = deploy(meta::contract_meta());
-    assert_eq!(item.name(), NAME);
-    assert_eq!(item.symbol(), SYMBOL);
-    assert_eq!(item.meta(), meta::contract_meta());
-    assert_eq!(item.total_supply(), U256::zero());
+    let (_, market_contract, _) = deploy(meta::contract_meta());
+    assert_eq!(market_contract.name(), NAME);
+    assert_eq!(market_contract.symbol(), SYMBOL);
+    assert_eq!(market_contract.meta(), meta::contract_meta());
+    assert_eq!(market_contract.total_supply(), U256::zero());
 }
 
 
 #[test]
 fn test_create_market_item() {
     let red_dragon = meta::red_dragon();
-    let (env, item, market_owner) = deploy(red_dragon);
+    let (env, market_contract, market_owner) = deploy(red_dragon);
     let user = env.next_user();
     let item_id = TokenId::from("1");
 
-    item.create_market_item(market_owner, user, item_id, get_nft_contract_hash(), U256::from("200000"), U256::from("1"));
-    let first_user_item = item.get_owned_item_by_index(Key::Account(user), U256::from(0));
-    let second_user_item = item.get_owned_item_by_index(Key::Account(user), U256::from(1));
+    market_contract.create_market_item(market_owner, user, item_id, get_nft_contract_hash(), U256::from("200000"), U256::from("1"));
+    let first_user_item = market_contract.get_owned_item_by_index(Key::Account(user), U256::from(0));
+    let second_user_item = market_contract.get_owned_item_by_index(Key::Account(user), U256::from(1));
     assert_eq!(first_user_item, Some(item_id));
-    assert_eq!(item.meta(), meta::red_dragon());
-    assert_eq!(item.item_nft_contract_address(item_id).unwrap(), get_nft_contract_hash());
-    assert_eq!(item.item_asking_price(item_id).unwrap(), U256::from("200000"));
-    assert_eq!(item.item_token_id(item_id).unwrap(), U256::from("1"));
-    assert_eq!(item.item_status(item_id).unwrap(), ITEM_STATUS_AVAILABLE);
-    assert_eq!(item.total_supply(), U256::one());
-    assert_eq!(item.balance_of(Key::Account(user)), U256::one());
+    assert_eq!(market_contract.meta(), meta::red_dragon());
+    assert_eq!(market_contract.item_nft_contract_address(item_id).unwrap(), get_nft_contract_hash());
+    assert_eq!(market_contract.item_asking_price(item_id).unwrap(), U256::from("200000"));
+    assert_eq!(market_contract.item_token_id(item_id).unwrap(), U256::from("1"));
+    assert_eq!(market_contract.item_status(item_id).unwrap(), ITEM_STATUS_AVAILABLE);
+    assert_eq!(market_contract.total_supply(), U256::one());
+    assert_eq!(market_contract.balance_of(Key::Account(user)), U256::one());
     assert_eq!(second_user_item, None);
-    assert_eq!(item.owner_of(item_id).unwrap(), Key::Account(user));
+    assert_eq!(market_contract.owner_of(item_id).unwrap(), Key::Account(user));
 }
 
 #[test]
 fn test_create_multiple_items() {
     let red_dragon = meta::red_dragon();
-    let (env, item, market_owner) = deploy(red_dragon);
+    let (env, market_contract, market_owner) = deploy(red_dragon);
     let user_1 = env.next_user();
     let user_2 = env.next_user();
     let item_id_1 = TokenId::zero();
@@ -89,20 +89,20 @@ fn test_create_multiple_items() {
     let nft_contract_hashes = vec![get_nft_contract_hash(), get_nft_contract_hash()];
     let item_asking_prices = vec![U256::from("200000"), U256::from("300000")];
     let item_token_ids = vec![U256::from("1"), U256::from("2")];
-    item.create_market_items(market_owner, user_1, item_ids.clone(), nft_contract_hashes.clone(), item_asking_prices.clone(), item_token_ids.clone());
-    item.create_market_item(market_owner, user_2, item_id_3.clone(), get_nft_contract_hash(), U256::from("400000"), U256::from("3"));
+    market_contract.create_market_items(market_owner, user_1, item_ids.clone(), nft_contract_hashes.clone(), item_asking_prices.clone(), item_token_ids.clone());
+    market_contract.create_market_item(market_owner, user_2, item_id_3.clone(), get_nft_contract_hash(), U256::from("400000"), U256::from("3"));
 
-    let user_1_item_1 = item.get_owned_item_by_index(Key::Account(user_1), U256::from("0"));
-    let user_1_item_2 = item.get_owned_item_by_index(Key::Account(user_1), U256::from("1"));
-    let user_2_item_1 = item.get_owned_item_by_index(Key::Account(user_2), U256::from("0"));
+    let user_1_item_1 = market_contract.get_owned_item_by_index(Key::Account(user_1), U256::from("0"));
+    let user_1_item_2 = market_contract.get_owned_item_by_index(Key::Account(user_1), U256::from("1"));
+    let user_2_item_1 = market_contract.get_owned_item_by_index(Key::Account(user_2), U256::from("0"));
 
     assert_eq!(user_1_item_1, Some(U256::from("0")));
     assert_eq!(user_1_item_2, Some(U256::from("1")));
     assert_eq!(user_2_item_1, Some(U256::from("2")));
 
-    assert_eq!(item.item_token_id(item_id_1).unwrap(), U256::from("1"));
-    assert_eq!(item.item_token_id(item_id_2).unwrap(), U256::from("2"));
-    assert_eq!(item.item_token_id(item_id_3).unwrap(), U256::from("3"));
+    assert_eq!(market_contract.item_token_id(item_id_1).unwrap(), U256::from("1"));
+    assert_eq!(market_contract.item_token_id(item_id_2).unwrap(), U256::from("2"));
+    assert_eq!(market_contract.item_token_id(item_id_3).unwrap(), U256::from("3"));
 
 }
 
@@ -110,18 +110,18 @@ fn test_create_multiple_items() {
 #[test]
 fn test_sell_market_item() {
     let red_dragon = meta::red_dragon();
-    let (env, item, market_owner) = deploy(red_dragon);
+    let (env, market_contract, market_owner) = deploy(red_dragon);
     let seller = env.next_user();
     let buyer = env.next_user();
     let item_id = TokenId::from("1");
 
-    item.create_market_item(market_owner, seller, item_id, get_nft_contract_hash(), U256::from("200000"), U256::from("1"));
-    let first_user_item = item.get_owned_item_by_index(Key::Account(seller), U256::from(0));
+    market_contract.create_market_item(market_owner, seller, item_id, get_nft_contract_hash(), U256::from("200000"), U256::from("1"));
+    let first_user_item = market_contract.get_owned_item_by_index(Key::Account(seller), U256::from(0));
     assert_eq!(first_user_item, Some(item_id));
-    assert_eq!(item.item_status(item_id).unwrap(), ITEM_STATUS_AVAILABLE);
+    assert_eq!(market_contract.item_status(item_id).unwrap(), ITEM_STATUS_AVAILABLE);
 
-    item.create_market_sale(market_owner, buyer, item_id);
-    assert_eq!(item.item_status(item_id).unwrap(), ITEM_STATUS_SOLD);
+    market_contract.create_market_sale(market_owner, buyer, item_id);
+    assert_eq!(market_contract.item_status(item_id).unwrap(), ITEM_STATUS_SOLD);
 
 }
 
