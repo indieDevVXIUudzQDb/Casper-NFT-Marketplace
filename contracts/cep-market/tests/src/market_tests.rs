7bc9837945c015a0c675e76835d625bbdef1d4ac
indieDevVXIUudzQDb <tokyoIndieDev@outlook.com>
2022-04-13 00:12:03 +0900

    call deployed contract

--- contracts/cep-market/tests/src/market_tests.rs
+++ contracts/cep-market/tests/src/market_tests.rs
@@ -1,19 +1,20 @@
+use std::borrow::Borrow;
+use casper_contract::contract_api::runtime::print;
 use casper_engine_test_support::{
     DeployItemBuilder, ExecuteRequestBuilder, InMemoryWasmTestBuilder, ARG_AMOUNT,
     DEFAULT_ACCOUNT_ADDR, DEFAULT_ACCOUNT_INITIAL_BALANCE, DEFAULT_GENESIS_CONFIG,
     DEFAULT_GENESIS_CONFIG_HASH, DEFAULT_PAYMENT,
 };
 use casper_execution_engine::core::engine_state::run_genesis_request::RunGenesisRequest;
-use casper_execution_engine::core::engine_state::GenesisAccount;
+use casper_execution_engine::core::engine_state::{ExecuteRequest, GenesisAccount};
 use std::collections::BTreeMap;
+use std::hash::Hash;
 use std::path::PathBuf;
 
 use casper_types::account::Account;
 use casper_types::CLType::String;
-use casper_types::{
-    account::AccountHash, runtime_args, ContractHash, HashAddr, Key, Motes, PublicKey, RuntimeArgs,
-    SecretKey, U256, U512,
-};
+use casper_types::{account::AccountHash, runtime_args, ContractHash, HashAddr, Key, Motes, PublicKey, RuntimeArgs, SecretKey, U256, U512, ContractPackage, CLTyped};
+use casper_types::bytesrepr::FromBytes;
 use cep47_tests::cep47_instance::CEP47Instance;
 
 use test_env::TestEnv;
@@ -286,6 +287,42 @@ const RUNTIME_ARG_NAME: &str = "message";
 const MARKET_CONTRACT_WASM: &str = "contract.wasm";
 const NFT_CONTRACT_WASM: &str = "cep47-token.wasm";
 
+pub fn fund_account(account: &AccountHash) -> ExecuteRequest {
+    let deploy_item = DeployItemBuilder::new()
+        .with_address(*DEFAULT_ACCOUNT_ADDR)
+        .with_authorization_keys(&[*DEFAULT_ACCOUNT_ADDR])
+        .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+        .with_transfer_args(runtime_args! {
+            "amount" => U512::from(30_000_000_000_000_u64),
+            "target" => *account,
+            "id" => <Option::<u64>>::None
+        })
+        .with_deploy_hash([1; 32])
+        .build();
+
+    ExecuteRequestBuilder::from_deploy_item(deploy_item).build()
+}
+
+pub fn query<T: FromBytes + CLTyped>(
+    builder: &InMemoryWasmTestBuilder,
+    base: Key,
+    path: &[std::string::String],
+) -> T {
+    builder
+        .query(None, base, path)
+        .expect("should be stored value.")
+        .as_cl_value()
+        .expect("should be cl value.")
+        .clone()
+        .into_t()
+        .expect("Wrong type in query result.")
+}
+pub fn contract_hash(name: std::string::String, account: AccountHash, builder: &InMemoryWasmTestBuilder) -> [u8; 32] {
+    let key = format!("{}_contract_hash_wrapped", name);
+    // query(builder, Key::Account(account), &[key])
+    query(builder, Key::Account(account), &[key])
+}
+
 #[test]
 fn should_store_hello_world() {
     // Create keypair.
@@ -301,25 +338,46 @@ fn should_store_hello_world() {
         None,
     );
 
+
+    let mut builder = InMemoryWasmTestBuilder::default();
     let mut genesis_config = DEFAULT_GENESIS_CONFIG.clone();
     genesis_config.ee_config_mut().push_account(account);
-
     let run_genesis_request = RunGenesisRequest::new(
         *DEFAULT_GENESIS_CONFIG_HASH,
         genesis_config.protocol_version(),
         genesis_config.take_ee_config(),
     );
+    builder.run_genesis(&run_genesis_request).commit();
+
+    let mut accounts = Vec::new();
+    for i in 0..10u8 {
+        let secret_key: SecretKey = SecretKey::ed25519_from_bytes([i; 32]).unwrap();
+        let public_key: PublicKey = (&secret_key).into();
+        let account_hash = AccountHash::from(&public_key);
+        accounts.push(account_hash);
+        builder
+            .exec(fund_account(&account_hash))
+            .expect_success()
+            .commit();
+    }
+
+
+
+    let reciever = accounts[1];
 
     let nft_deploy_item = DeployItemBuilder::new()
         .with_empty_payment_bytes(runtime_args! {
             ARG_AMOUNT => *DEFAULT_PAYMENT
         })
-        .with_session_code(PathBuf::from(NFT_CONTRACT_WASM), runtime_args! {
-        "name" => MARKET_NAME,
-        "symbol" => SYMBOL,
-        "meta" => meta::contract_meta(),
-        "contract_name" => MARKET_NAME,
-    })
+        .with_session_code(
+            PathBuf::from(NFT_CONTRACT_WASM),
+            runtime_args! {
+                "name" => NAME,
+                "symbol" => SYMBOL,
+                "meta" => meta::contract_meta(),
+                "contract_name" => NAME,
+            },
+        )
         .with_authorization_keys(&[account_addr])
         .with_address(account_addr)
         .build();
@@ -330,21 +388,23 @@ fn should_store_hello_world() {
         .with_empty_payment_bytes(runtime_args! {
             ARG_AMOUNT => *DEFAULT_PAYMENT
         })
-        .with_session_code(PathBuf::from(MARKET_CONTRACT_WASM), runtime_args! {
-        "market_name" => MARKET_NAME,
-        "market_symbol" => SYMBOL,
-        "market_meta" => meta::contract_meta(),
-        "contract_name" => MARKET_NAME,
-        RUNTIME_ARG_NAME => VALUE,
-    })
+        .with_session_code(
+            PathBuf::from(MARKET_CONTRACT_WASM),
+            runtime_args! {
+                "market_name" => MARKET_NAME,
+                "market_symbol" => SYMBOL,
+                "market_meta" => meta::contract_meta(),
+                "contract_name" => MARKET_NAME,
+                RUNTIME_ARG_NAME => VALUE,
+            },
+        )
         .with_authorization_keys(&[account_addr])
         .with_address(account_addr)
         .build();
 
-    let market_execute_request = ExecuteRequestBuilder::from_deploy_item(market_deploy_item).build();
+    let market_execute_request =
+        ExecuteRequestBuilder::from_deploy_item(market_deploy_item).build();
 
-    let mut builder = InMemoryWasmTestBuilder::default();
-    builder.run_genesis(&run_genesis_request).commit();
 
     // prepare assertions.
     let result_of_query = builder.query(
@@ -355,20 +415,59 @@ fn should_store_hello_world() {
     assert!(result_of_query.is_err());
 
     // deploy the contract.
-    builder.exec(nft_execute_request).commit().expect_success();
-    builder.exec(market_execute_request).commit().expect_success();
+    let res_1 = builder.exec(nft_execute_request).commit().expect_success();
+    let nft_contract_addressOption= res_1.query(
+        None,
+        Key::Account(account_addr),
+        &["contract_package_hash".to_string()],
+    ).unwrap();
 
-    // make assertions
-    let result_of_query = builder
-        .query(None, Key::Account(account_addr), &[KEY.to_string()])
-        .expect("should be stored value.")
-        .as_cl_value()
-        .expect("should be cl value.")
-        .clone()
-        .into_t::<std::string::String>()
-        .expect("should be string.");
+    let nft_contract_hash = nft_contract_addressOption.as_contract_package().unwrap();
 
-    assert_eq!(result_of_query, VALUE);
+    // println!("res_1 {:?}", res_1.get_exec_results());
+    // println!("nft_contract_address {:?}", nft_contract_hash.current_contract_hash());
+
+    // let res_2 = builder
+    //     .exec(market_execute_request)
+    //     .commit()
+    //     .expect_success();
+
+
+    // // make assertions
+    // let result_of_query = builder
+    //     .query(None, Key::Account(account_addr), &[KEY.to_string()])
+    //     .expect("should be stored value.")
+    //     .as_cl_value()
+    //     .expect("should be cl value.")
+    //     .clone()
+    //     .into_t::<std::string::String>()
+    //     .expect("should be string.");
+    //
+    // assert_eq!(result_of_query, VALUE);
+
+    let deploy_item_builder = DeployItemBuilder::new()
+        .with_empty_payment_bytes(runtime_args! {
+            ARG_AMOUNT => *DEFAULT_PAYMENT
+        })
+        .with_authorization_keys(&[account_addr])
+        .with_address(account_addr);
+
+    let contract_hash_bytes = contract_hash(NAME.to_string(),account_addr, &builder);
+    let contract_hash = ContractHash::from(contract_hash_bytes);
+    let deploy_mint = deploy_item_builder.with_stored_session_hash(
+        contract_hash,
+        "mint",
+        runtime_args! {
+                "recipient" => Key::Account(account_addr),
+                "token_ids" => vec![TokenId::from("1")],
+                "token_metas" => vec![meta::red_dragon()],
+        }
+    ).build();
+    let nft_mint_execute_request =
+        ExecuteRequestBuilder::from_deploy_item(deploy_mint).build();
+    builder.exec(nft_mint_execute_request).commit().expect_success();
+
+
 }
 
 #[ignore]
