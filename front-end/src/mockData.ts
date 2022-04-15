9f6052e89ef0810dec39172a3910cf0c7773827a
indieDevVXIUudzQDb <tokyoIndieDev@outlook.com>
2022-04-16 03:17:47 +0900

    add some mock data and images

---
+++ front-end/src/mockData.ts
@@ -0,0 +1,124 @@
+export const mockData = {
+    "planets": [
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/1.png",
+            "name": "Byotau"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/2.png",
+            "name": "Gullveig"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/3.png",
+            "name": "Wogni"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/4.png",
+            "name": "Scatha VI"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/5.png",
+            "name": "Iahamu"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/6.png",
+            "name": "Honi VI"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/7.png",
+            "name": "Menhit III"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/8.png",
+            "name": "Booly Tera"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/9.png",
+            "name": "Luthien IV,"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/10.png",
+            "name": "New Zion"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/11.png",
+            "name": "New Triton"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/12.png",
+            "name": "New Japan"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/13.png",
+            "name": "New Vaalbara"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/14.png",
+            "name": "Kothar V"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/15.png",
+            "name": "New Pallas"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/16.png",
+            "name": "Jakri"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/17.png",
+            "name": "Irkek"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/18.png",
+            "name": "Orwer,"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/19.png",
+            "name": "Orthrus II"
+        },
+        {
+            "description": "Unique Planet",
+            "actionText": "Buy",
+            "url": "assets/planets/20.png",
+            "name": "New Vaalbara"
+        }
+    ]
+}
