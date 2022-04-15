3c75660c1863fd825d59969c27ea3b2cb39cc964
indieDevVXIUudzQDb <tokyoIndieDev@outlook.com>
2022-04-16 07:04:45 +0900

    add casper utils and link casper signer

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   AppShell,
@@ -9,7 +9,8 @@ import {
   MediaQuery,
   Burger,
   useMantineTheme,
-  SimpleGrid, Title,
+  SimpleGrid,
+  Title,
 } from "@mantine/core";
 import { Wallet } from "tabler-icons-react";
 
@@ -18,8 +19,16 @@ import { mockData } from "../mockData";
 import styles from "../styles/dashboard-cyber.module.scss";
 import MainLinks from "./_mainLinks";
 import User from "./_user";
+import {
+  accountInformation,
+  EVENT_STREAM_ADDRESS,
+  getActiveAccountBalance,
+  subscribeToContractEvents,
+} from "../utils/cep47_utils";
+import { EventStream } from "casper-js-sdk";
+import { isConnected } from "casper-js-sdk/dist/lib/Signer";
 
-const CustomHeader = () => {
+const CustomHeader = (props: { address: string }) => {
   const [opened, setOpened] = useState(false);
 
   const theme = useMantineTheme();
@@ -48,6 +57,7 @@ const CustomHeader = () => {
         </div>
         <div />
         <ActionIcon variant="default" size={30}>
+          {props.address}
           <Wallet size={16} />
         </ActionIcon>
       </Group>
@@ -55,7 +65,7 @@ const CustomHeader = () => {
   );
 };
 
-const CustomNavbar = () => {
+const CustomNavbar = (props: { connected: boolean }) => {
   return (
     <Navbar
       p="md"
@@ -72,7 +82,7 @@ const CustomNavbar = () => {
             <MainLinks />
           </Navbar.Section>
           <Navbar.Section>
-            <User />
+            <User connected={props.connected} />
           </Navbar.Section>
         </div>
         <div className={styles.lineRight}>
@@ -84,8 +94,39 @@ const CustomNavbar = () => {
 };
 
 export default function DashboardCyber() {
+  const [address, setAddress] = useState("");
+  const [publicKey, setPublicKey] = useState("");
+  const [balance, setBalance] = useState("");
+  const [nftBalance, setNFTBalance] = useState(0);
+  const [tx, setTx] = useState("");
+  const [to, setTo] = useState("");
+  const [amount, setAmount] = useState("");
+  const [connected, setConnected] = useState(false);
+  const updateAccountInformation = async () => {
+    const {
+      textAddress,
+      textBalance,
+      publicKey: updatedPublicKey,
+    } = await accountInformation();
+    setAddress(textAddress);
+    setBalance(textBalance);
+    setPublicKey(updatedPublicKey);
+    setNFTBalance(await getActiveAccountBalance());
+    setConnected(true);
+  };
+
+  useEffect(() => {
+    console.log("subscription called");
+    const es = new EventStream(EVENT_STREAM_ADDRESS!);
+    subscribeToContractEvents(es, () => getActiveAccountBalance());
+    updateAccountInformation();
+  }, []);
   return (
-    <AppShell padding="md" navbar={<CustomNavbar />} header={<CustomHeader />}>
+    <AppShell
+      padding="md"
+      navbar={<CustomNavbar connected={connected} />}
+      header={<CustomHeader address={address} />}
+    >
       <Title order={1}>Distant Planet Collection</Title>
 
       <SimpleGrid cols={3} spacing={50} style={{ margin: "5em" }}>
